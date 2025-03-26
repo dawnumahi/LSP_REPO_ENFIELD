8fdc48138b89cf19e5ae16827caecbacc9cbe95a
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-03-26 21:18:41 +0000

    driver

--- LSP_Spring2025/src/org/howard/edu/lsp/assignment5/Driver.java
+++ LSP_Spring2025/src/org/howard/edu/lsp/assignment5/Driver.java
@@ -6,22 +6,85 @@ public class Driver {
         set1.add(1);
         set1.add(2);
         set1.add(3);
-
-        System.out.println("Value of Set1 is:" + set1.toString()); 
-        System.out.println("Smallest value in Set1 is:" + set1.smallest());
-        System.out.println("Largest value in Set1 is:" + set1.largest());
+        set1.add(4);
 
         IntegerSet set2 = new IntegerSet();
         set2.add(3);
         set2.add(4);
         set2.add(5);
+        set2.add(6);
+        set2.add(7);
 
-        System.out.println("Union of Set1 and Set2");
-        System.out.println("Value of Set1 is:" + set1.toString());
-        System.out.println("Value of Set2 is:" + set2.toString());
+        // Testing tostring
+        System.out.println("Set 1:" + set1.toString()); 
+        System.out.println("Set 2:" + set2.toString());
+
+        //Testing smallest
+        System.out.println("Smallest value in Set 1 is:" + set1.smallest());
+        System.out.println("Smallest value in Set 2 is:" + set2.smallest());
+
+        //Testing largest
+        System.out.println("Largest value in Set 1 is:" + set1.largest());
+        System.out.println("Largest value in Set 2 is:" + set2.largest());
+       
+        //Testing length
+        System.out.println("Length of Set 1: " + set1.length());
+        System.out.println("Length of Set 2: " + set2.length());
+
+        //Testing add
+        System.out.println("Adding 5 to Set 1...");
+        set1.add(5);
+        System.out.println("Adding 8 to Set 2...");
+        set2.add(8);
+
+        //testing remove
+        System.out.println("Removing 11 from Set 1..."); 
+        set1.remove(11);
+        System.out.println("Removing 8 from Set 2..."); 
+        set2.remove(8);
+        System.out.println("Removing 9 from Set 2..."); //There is no 9 in set 2
+        set2.remove(9);
+        System.out.println("Set 1:" + set1.toString());
+        System.out.println("Set 2:" + set2.toString());
+
+        //Contains
+        System.out.println("Does Set 1 contain 2? " + set1.contains(2));
+        System.out.println("Does Set 1 contain 5? " + set1.contains(5));
+        System.out.println("Does Set 2 contain 9? " + set1.contains(9));
+
+        //Intersect
+        System.out.println("Intersection of Set 1 and Set 2");
+        set1.intersect(set2);
+        System.out.println("Set 1 after intersection: " + set1);
+
+        //Clear
+        System.out.println("Clearing Set 1");
+        set1.clear();
+        System.out.println("Set 1 after clear: " + set1);
+
+        // Testing isEmpty
+        System.out.println("Is Set1 empty? " + set1.isEmpty());
+
+        System.out.println("Adding 1, 2, 3, 4 to Set 1...");
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+
+        System.out.println("Set 1: " + set1);
+        // Testing isEmpty
+        System.out.println("Is Set1 empty? " + set1.isEmpty());
+
+        // Test equals 
+        System.out.println("Does Set 1 equal Set 2? " + set1.equals(set2));
+
+        //Union and toString
+        System.out.println("Union of Set 1 and Set 2");
+        System.out.println("Value of Set 1 is:" + set1.toString());
+        System.out.println("Value of Set 2 is:" + set2.toString());
         set1.union(set2);	// union of set1 and set2
-        System.out.println("Result of union of Set1 and Set2: " +  set1.toString());
-       	// result of union of set1 and set2
+        System.out.println("Result of union of Set 1 and Set 2: " +  set1.toString());
+       	
 
     }
 }
