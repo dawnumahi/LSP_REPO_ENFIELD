bb34f68485ea629febfd84785693b51140fed8be
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-08 19:49:39 -0400

    Ass1 initial commit.

---
+++ LSP_Spring2025/src/org/howard/edu/lsp/assignment6/IntegerSetException.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment6;
+
+public class IntegerSetException extends Exception{
+    /**
+     * Constructor for IntegerSetException
+     * 
+     * @param message
+     */
+    public IntegerSetException(String message) {
+        super(message);
+    }
+}
