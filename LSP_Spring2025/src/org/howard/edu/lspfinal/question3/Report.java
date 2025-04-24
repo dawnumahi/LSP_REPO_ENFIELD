706013bbf67e8da1d6e784ea7cbeba385ec21200
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:06:57 -0400

    Report template

--- LSP_Spring2025/src/org/howard/edu/lspfinal/question3/Report.java
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question3/Report.java
@@ -3,10 +3,21 @@ package org.howard.edu.lspfinal.question3;
 /**
  * Defining the template for generating any report. 
  */
-public class Report {
-    public void generateReport(){
+public abstract class Report {
+    /**
+     * Workflow: Load, format, and print data. 
+     */
 
+    public void generateReport(){
+        loadData();
+        formatData();
+        printReport();
+        System.out.println();
     }
         
-    
+    protected abstract void loadData();
+    protected abstract void formatData();
+    protected abstract void printReport();
+
+    //Source: https://refactoring.guru/design-patterns/template-method/java/example
 }
