b560144f0dd837823690b297d8bb5191c054f6f0
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:18:19 -0400

    sales report

--- LSP_Spring2025/src/org/howard/edu/lspfinal/question3/SalesReport.java
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question3/SalesReport.java
@@ -1,5 +1,23 @@
 package org.howard.edu.lspfinal.question3;
 
-public class SalesReport {
-    
+/**
+ * Creating a report for the Sales department.
+ */
+public class SalesReport extends Report {
+    @Override
+    protected void loadData(){
+        System.out.println("Loading sales data...");
+    }
+
+    @Override
+    protected void formatData(){
+        System.out.println("Formatting sales data...");
+    }
+
+    @Override
+    protected void printReport(){
+        System.out.println("Printing sales data...");
+    }
+
+    //Source: https://refactoring.guru/design-patterns/template-method/java/example
 }
