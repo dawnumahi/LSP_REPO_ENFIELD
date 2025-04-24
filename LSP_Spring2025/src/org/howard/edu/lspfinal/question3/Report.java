538f0efdcb2a2eaf47ca53f2b7f8189cb9daf275
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:00:44 -0400

    first commit

---
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question3/Report.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lspfinal.question3;
+
+/**
+ * Defining the template for generating any report. 
+ */
+public class Report {
+    public void generateReport(){
+
+    }
+        
+    
+}
