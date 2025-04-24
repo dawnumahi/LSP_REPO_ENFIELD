3abfaffc0872709301c5e07d9cdd5b7832202c35
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:32:46 -0400

    Question 2.

---
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question2/Task.java
@@ -0,0 +1,38 @@
+package org.howard.edu.lspfinal.question2;
+/**
+ * Class for an individual Task with a name, priority, and status.
+ */
+public class Task {
+    private final String taskName;
+    private final int priority;
+    private String status; 
+
+    /**
+     * Constructor for a new Task. 
+     * @param name
+     * @param priority
+     * @param status
+     */
+    public Task(String name, int priority, String status) {
+        this.taskName = name;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public String getName(){
+        return this.taskName;
+    }
+
+    public int getPriority(){
+        return this.priority;
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(String s){
+        this.status = s;
+    }
+
+}
