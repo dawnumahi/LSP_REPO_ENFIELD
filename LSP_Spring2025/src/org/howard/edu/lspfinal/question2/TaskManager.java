544ccbbfe2884bce17f4407c38cb0edd264ea324
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:43:27 -0400

    Add tasks.

--- LSP_Spring2025/src/org/howard/edu/lspfinal/question2/TaskManager.java
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question2/TaskManager.java
@@ -1,5 +1,23 @@
 package org.howard.edu.lspfinal.question2;
+import java.util.*;
 
+/**
+ * Task manager to add, retrieve, update, and print tasks. 
+ */
 public class TaskManager {
+    private Map<String, Task> tasks = new HashMap<>();
     
+    /**
+     * Adds a new task with a unique name.
+     * @param n
+     * @param p
+     * @param s
+     * @throws DuplicateTaskException
+     */
+    public void addTask(String n, int p, String s) throws DuplicateTaskException{
+        if(tasks.containsKey(n)){
+            throw new DuplicateTaskException("Task '" + n + "' already exists.");
+        }
+        tasks.put(n, new Task(n, p, s));
+    }
 }
