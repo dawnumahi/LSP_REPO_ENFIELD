23390d3abf3e603fe45531263c93d4bf1fbcff6d
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:54:38 -0400

    Update status, print grouped by status.

--- LSP_Spring2025/src/org/howard/edu/lspfinal/question2/TaskManager.java
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question2/TaskManager.java
@@ -20,4 +20,46 @@ public class TaskManager {
         }
         tasks.put(n, new Task(n, p, s));
     }
+
+    /**
+     * Method to get a tesk by name.
+     * @param n
+     * @return Returns the task.
+     * @throws TaskNotFoundException
+     */
+    public Task retrieveTask(String n) throws TaskNotFoundException{
+        Task t = tasks.get(n);
+        if (t == null){
+            throw new TaskNotFoundException( " " + n + "' not found.");
+        }
+        return t;
+    }
+
+    /**
+     * Updates the status of the task named.
+     * @param n
+     * @param updatedStatus
+     * @throws TaskNotFoundException
+     */
+    public void updateStatus(String n, String updatedStatus) throws TaskNotFoundException{
+        Task t = retrieveTask(n);
+        t.setStatus(updatedStatus);
+    }
+
+   /**
+    * Groups tasks by status and prints them out.
+    */
+    public void printTasksGroupedByStatus() {
+        System.out.println("Tasks grouped by status:");
+        List<String> statuses = Arrays.asList("TODO", "IN_PROGRESS", "DONE");
+        for (String status : statuses) {
+            System.out.println(status + ": ");
+            for (Task t : tasks.values()) {
+                if (t.getStatus().equals(status)) {
+                    System.out.println("  " + t);
+                }
+            }
+        }
+    }
+
 }
