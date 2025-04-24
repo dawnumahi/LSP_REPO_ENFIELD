257c13e67c94a9eb1da5d06cb70cae65b9db0159
dawnumahi <143527732+dawnumahi@users.noreply.github.com>
2025-04-24 19:17:53 -0400

    Driver and inventory report.

--- LSP_Spring2025/src/org/howard/edu/lspfinal/question3/Driver.java
+++ LSP_Spring2025/src/org/howard/edu/lspfinal/question3/Driver.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lspfinal.question3;
 
 public class Driver {
-    
+    public static void main(String[] args) {
+        Report inventory = new InventoryReport();
+        inventory.generateReport();
+
+        Report sales = new SalesReport();
+        sales.generateReport();
+    }
 }
